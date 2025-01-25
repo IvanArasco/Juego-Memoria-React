1e1b2cff39ccbee3be3108799312918d503bfbbc
Ivan Arasco <arascoivan97@gmail.com>
2025-01-25 22:37:55 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/juego-memoria-react/', 
+  base: '/Juego-Memoria-React/', 
   plugins: [react()],
 })
