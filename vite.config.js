4c7e2bda561e171804f9dd0d3aca230e96962f5c
Ivan Arasco <arascoivan97@gmail.com>
2025-01-26 00:42:45 +0100

    Change vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Juego-Memoria-React/', 
+  base: 'https://ivanarasco.github.io/Juego-Memoria-React/', 
   plugins: [react()],
 })
